091d771c3922915909d24e04617aad897e7bd3b4
Ruthvik Reddy <2003.ruthvik@gmail.com>
2023-04-11 01:21:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const app = express();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use("/", async (req, res) => {
+  let query = req.query;
+
+  console.log(query);
+  let myurl = new URL("https://muerp.mahindrauniversity.edu.in/markAtt.json");
+  const params = myurl.searchParams;
+  console.log(query.at);
+  console.log(query.ld);
+  params.append("at", query.at);
+  params.append("ld", query.ld);
+
+  try {
+    console.log(myurl.toString());
+    const response = await axios.get(myurl.toString());
+    console.log(response);
+    // if (!response.ok) {
+    //   return res.status(200).json();
+    // } else {
+    // const data = await response.json();
+    return res.status(200).json(response.data);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json(err);
+  }
+});
+app.listen(8080);
